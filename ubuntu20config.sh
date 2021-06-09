bb2de3a4df33b630cda4b1108dff194fca5175b9
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-06-08 20:20:31 -0400

    Update ubuntu20config.sh

--- ubuntu20config.sh
+++ ubuntu20config.sh
@@ -4,7 +4,7 @@ apt -y update
 
 apt -y upgrade
 
-apt -y install golang-go docker.io curl apache2-utils default-jre default-jdk wget git vim nano make g++ libsctp-dev lksctp-tools net-tools iproute2 libssl-dev tcpdump curl jq chromium-browser iputils-ping apt-transport-https
+apt -y install golang-go docker.io curl apache2-utils default-jre default-jdk wget git vim nano make g++ libsctp-dev lksctp-tools net-tools iproute2 libssl-dev tcpdump curl jq chromium-browser iputils-ping apt-transport-https nghttp2-client
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -29,4 +29,10 @@ git clone -b v3.2.0 https://github.com/aligungr/UERANSIM && cd UERANSIM && make
 
 cp build/nr-* /usr/local/bin/
 
+curl https://baltocdn.com/helm/signing.asc | apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt -y update
+apt-get install helm
+
+curl -sL https://run.linkerd.io/install | sh
+export PATH=$PATH:/users/umakant/.linkerd2/bin
