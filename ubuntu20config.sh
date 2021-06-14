ff401dc81fea78c39ecf630cd15b2cbbe742478b
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-06-13 23:11:09 -0700

    Update ubuntu20config.sh

--- ubuntu20config.sh
+++ ubuntu20config.sh
@@ -4,7 +4,7 @@ apt -y update
 
 apt -y upgrade
 
-apt -y install docker.io golang-go curl apache2-utils default-jre default-jdk wget git vim nano make g++ libsctp-dev lksctp-tools net-tools iproute2 libssl-dev tcpdump curl jq iputils-ping apt-transport-https nghttp2-client bash-completion xauth
+apt -y install docker.io curl apache2-utils default-jre default-jdk wget git vim nano make g++ libsctp-dev lksctp-tools net-tools iproute2 libssl-dev tcpdump curl jq iputils-ping apt-transport-https nghttp2-client bash-completion xauth gcc autoconf libtool pkg-config libmnl-dev libyaml-dev
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -49,3 +49,28 @@ git clone https://github.com/free5gc/free5gc
 cd k8s
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt -y install ./google-chrome-stable_current_amd64.deb
+
+cd ~/.
+apt -y remove golang-go
+rm -rf /usr/local/go
+wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+tar -C /usr/local -zxvf go1.14.4.linux-amd64.tar.gz
+rm -rf go1.14.4.linux-amd64.tar.gz
+mkdir -p ~/go/{bin,pkg,src}
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin:$GOROOT/bin' >> ~/.bashrc
+echo 'export GO111MODULE=auto' >> ~/.bashrc
+source ~/.bashrc
+apt -y update
+go get -u github.com/sirupsen/logrus
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o eno49 -j MASQUERADE
+iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
+systemctl stop ufw
+
+cd /opt/
+git clone -b v0.2.1 https://github.com/free5gc/gtp5g.git
+cd gtp5g
+make
+make install
