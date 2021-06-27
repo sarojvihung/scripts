05718446080e34de8abd5c6246f7be11e128af83
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-06-26 20:30:07 -0400

    Update configWorkerNode.sh

--- configWorkerNode.sh
+++ configWorkerNode.sh
@@ -6,12 +6,6 @@ if [[ $# -ne 1 ]] ; then
 fi
 
 kjoincmd="$1"
-sh /opt/scripts/dockerInstall.sh
-systemctl enable docker.service
-swapoff -a
-kubeadm reset --force
-systemctl restart kubelet
-eval $kjoincmd
 echo "alias lgc='google-chrome --no-sandbox --disable-gpu'" >> ~/.bashrc
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export GOROOT=/usr/local/go" >> ~/.bashrc
@@ -19,3 +13,9 @@ source ~/.bashrc
 echo "export PATH=$PATH:$GOPATH/bin:$GOROOT/bin" >> ~/.bashrc
 echo "export GO111MODULE=auto" >> ~/.bashrc
 source ~/.bashrc
+sh /opt/scripts/dockerInstall.sh
+systemctl enable docker.service
+swapoff -a
+kubeadm reset --force
+systemctl restart kubelet
+eval $kjoincmd
