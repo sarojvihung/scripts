c6cbd1845a035ea4fcbedec1fa5b921ed1e51501
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-07-20 18:20:09 -0400

    webui

---
+++ configWebuiPod.sh
@@ -0,0 +1,3 @@
+apk update
+apk add openssh tcpdump tshark
+tcpdump -i any -w ui.pcapng
--- createSubsOpen5gs.sh
+++ createSubsOpen5gs.sh
@@ -6,10 +6,21 @@ PORT=32539
 username=admin
 password=1423
 
+if [ "$#" -eq 2 ]; then
+  framenum=$(tshark -r ui.pcapng -Y 'http && http.request.uri=="/api/db/Subscriber"' | sed -n '1p' | awk '{print $1}')
+  cookie=$(tshark -r ui.pcapng -Y frame.number==$framenum -T json | grep '"http.cookie": "connect.sid=' | awk -v FS='(sid=|",)' '{print $2}')
+  auth=$(tshark -r ui.pcapng -Y frame.number==$framenum -T json | grep "Authorization: Bearer" | awk -v FS='(Bearer |\r\n",)' '{print $2}' | sed 's/.\{6\}$//')
+  xcrf=$(tshark -r ui.pcapng -Y frame.number==$framenum -T json | grep "X-CSRF-TOKEN:" | awk -v FS='(X-CSRF-TOKEN: |\r\n",)' '{print $2}' | sed 's/.\{6\}$//')
+else 
+  cookie="s%3Ad5xw8H8mdjxx61rwlH2mp8p1id_rxApP.XHgFK7hKXjOmgt8RBrdXBQjMV4B6Mj4%2F8tSJwMdSNIA"
+  auth="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYwZjUwOTVhNDgwYjI5MDAxOWE3ZjIzNCIsInVzZXJuYW1lIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNjI2NzE4NjcxfQ.hqsVKeDTwHe0VsSE7Oaz4aD9ARxYBeCjRg_aX7wLmvU"
+  xcrf="41XLxwXizPGQuRCHgobvFKfOEhqvV9Ea4xwjI="
+fi
+
 ContentTypeHeader="Content-Type: application/json"
-CookieHeader="Cookie: connect.sid=s%3AuoN9LIawCzzhpqAEGcA1-w0rs9TXz-8m.7aDO%2F8zQSzieLJR%2BE9c0g%2BAA4z9AijdAbW8ZftIvgu8"
-AuthHeader="Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYwYjU4MDg1YjM2N2FjMDAxOThjMzg3YiIsInVzZXJuYW1lIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNjIyNTA4Nzg2fQ.4tNgvkinGqqYfkGe-371tnZwwvAhQGlmxvIE6cUw2hQ"
-XCSRFTOKENHeader="X-CSRF-TOKEN: eBjhr7PLxbgnAHJJ4YcmCU2WxxY1tYBPo399U="
+CookieHeader="Cookie: connect.sid=$cookie"
+AuthHeader="Authorization: Bearer $auth"
+XCSRFTOKENHeader="X-CSRF-TOKEN: $xcrf"
 URI="http://$IP:$PORT/api/db/Subscriber"
 
 jsonStr='{"imsi":"208930000000027","security":{"k":"465B5CE8 B199B49F AA5F0A2E E238A6BC","amf":"8000","op_type":0,"op_value":"E8ED289D EBA952E4 283B54E8 8E6183CA","op":null,"opc":"E8ED289D EBA952E4 283B54E8 8E6183CA"},"ambr":{"downlink":{"value":1,"unit":3},"uplink":{"value":1,"unit":3}},"slice":[{"sst":1,"default_indicator":true,"session":[{"name":"internet","type":3,"pcc_rule":[],"ambr":{"uplink":{"value":1,"unit":3},"downlink":{"value":1,"unit":3}},"qos":{"index":9,"arp":{"priority_level":8,"pre_emption_capability":1,"pre_emption_vulnerability":1}}}]}],"schema_version":1}'
