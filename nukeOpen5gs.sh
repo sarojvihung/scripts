3ff84a66ba449e85a7659db1cf405621502da648
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-08-03 20:22:30 -0400

    fix nuke

--- nukeOpen5gs.sh
+++ nukeOpen5gs.sh
@@ -10,6 +10,7 @@ if [ "$Hostname" = "wabash" ] ; then
     cd /home/ukulkarn/opensource-5g-core/helm-chart/ 
 else
     cd /opt/opensource-5g-core/helm-chart/ 
+fi
 helm -n open5gs install -f values.yaml 5gcore ./
 sleep 10
 kubectl config set-context --current --namespace=open5gs
