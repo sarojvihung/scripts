972e28ccf203f86bfcdb62cd58b8c16633e28d65
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-08-05 20:57:54 -0400

    change port

--- startUeCalls.sh
+++ startUeCalls.sh
@@ -5,7 +5,7 @@ for var in "$@"
 do
     if [[ $var =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         echo "UE-SIM IP Address is $var"
-        cmd="curl --verbose --request POST --header \"Content-Type:application/json\" --data '{\"numSessions\":\"$numSession\"}'  http://$var:80"
+        cmd="curl --verbose --request POST --header \"Content-Type:application/json\" --data '{\"numSessions\":\"$numSession\"}'  http://$var:15692"
         echo "CMD is $cmd"
         eval "$cmd > /dev/null 2>&1 &"
     else
--- ueSimServer.py
+++ ueSimServer.py
@@ -38,7 +38,7 @@ class S(BaseHTTPRequestHandler):
         return
 
 
-def run(server_class=HTTPServer, handler_class=S, port=80):
+def run(server_class=HTTPServer, handler_class=S, port=15692):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print('Starting httpd...')
