6c9ef0ca289900742de434cfa59b99af3fa6fefd
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-08-05 21:34:08 -0400

    columns

--- mongoStats.sh
+++ mongoStats.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 mongostat >> mongo_stats.txt
--- topFile.sh
+++ topFile.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 while true
 do
         date >> top_data.txt
-        top -b -n 1 >> top_data.txt
+        COLUMNS=500 top -b -n 1 >> top_data.txt
         echo " " >> top_data.txt
         sleep 0.2
 done
