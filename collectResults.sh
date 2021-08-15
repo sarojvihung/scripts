ad128a56134e265bb02f743c530e081353ac6c24
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-08-14 22:31:43 -0400

    fix collect

--- collectResults.sh
+++ collectResults.sh
@@ -31,19 +31,22 @@ do
     done
 done
 
-for uexp in /opt/Ue_Ops/* ; 
+for uexp in /opt/Ue_Ops/* ;
 do
     f1=`basename $uexp`
-    for exp in $uexp/* ; 
+    for exp in $uexp/* ;
     do
         f2=`basename $exp`
-        for subexp in $exp/* ; 
+        for subexp in $exp/* ;
         do
-            
             f3=`basename $subexp`
             mkdir -p /opt/Results/$f2/$f3/
-            cmd="scp -o StrictHostKeyChecking=no $subexp/* /opt/Results/$f2/$f3/"
-            eval "$cmd"
+            for filename in $subexp/* ; 
+            do
+                f4=`basename $filename`
+                cmd="scp -o StrictHostKeyChecking=no $subexp/$f4 /opt/Results/$f2/$f3/${f4}_${f1}"
+                eval "$cmd"
+            done
         done
     done
 done
