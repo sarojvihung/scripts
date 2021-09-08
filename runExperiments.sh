1b28bf74040e86eeebbd1cd8e89b2a09b6e2ad4e
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-09-07 20:22:09 -0400

    top 10

--- runExperiments.sh
+++ runExperiments.sh
@@ -5,7 +5,7 @@ if [[ $# -ne 1 ]] ; then
     exit 1
 fi
 
-experimentDir="$1"
+experimentDirPrefix="$1"
 numWorkerNodes=10
 
 mongoPod=`kubectl -n open5gs get po -o json |  jq '.items[] | select(.metadata.name|contains("open5gs"))| .metadata.name' | grep "mongo" | sed 's/"//g'`
@@ -13,71 +13,76 @@ kubectl exec -it $mongoPod -- bash -c "apt-get update && apt -y install git vim 
 
 declare -a subDir=("100" "200" "300" "400" "500" "600" "700" "800" "900" "1000")
 
+declare -a experimentDirAry=("$experimentDirPrefix-1" "$experimentDirPrefix-2" "$experimentDirPrefix-3" "$experimentDirPrefix-4" "$experimentDirPrefix-5" "$experimentDirPrefix-6" "$experimentDirPrefix-7" "$experimentDirPrefix-8" "$experimentDirPrefix-9" "$experimentDirPrefix-10")
+
 declare -a ueNodes=("10.10.1.13" "10.10.1.15")
 
-for pcsDir in "${subDir[@]}"
+for experimentDir in "${experimentDirAry[@]}"
 do
-
-    echo "Sub-dir is $pcsDir"
-    numSessions=$(( pcsDir / 2 ))
-
-    #cleanup
-    kubectl get pods --no-headers=true | awk '/upf|amf|bsf|pcf|udm|ausf|nrf|nssf|udr|smf/{print $1}'| xargs  kubectl delete pod
-
-    sleep 60
-
-
-    #start-ran
-    bash /opt/scripts/runNodeCmd.sh "nr-gnb -c /opt/UERANSIM/config/open5gs/gnb.yaml > /dev/null 2>&1 &" 11 13
-
-    #bash /opt/scripts/runNodeCmd.sh "/opt/scripts/launchUeSim.py > /dev/null 2>&1 &" 12 14
-
-
-    #start-monitoring
-    #kubectl exec $mongoPod -- bash -c "/scripts/mongoMonitor.py" &
-    mongoPodIp=$(kubectl get pod $mongoPod --template={{.status.podIP}})
-    curl --verbose --request POST --header "Content-Type:application/json" --data '{"expDir":"'$experimentDir'","subExpDir":"'$pcsDir'","runTime":30}' http://$mongoPodIp:15692
-    
-    bash /opt/scripts/startTop.sh $numWorkerNodes $experimentDir $pcsDir
-
-
-    #start-ue
-    for ueNodeIp in "${ueNodes[@]}"
+    for pcsDir in "${subDir[@]}"
     do
-        if (( pcsDir > 500 )); then
-            sleep 0.3
-        else
-            sleep 0.2
-        fi
-        echo "UE-SIM IP Address is $ueNodeIp"
-        curl --verbose --request POST --header "Content-Type:application/json" --data '{"numSessions":"'$numSessions'","expDir":"'$experimentDir'","subExpDir":"'$pcsDir'"}'  http://$ueNodeIp:15692
+
+        echo "Sub-dir is $pcsDir"
+        numSessions=$(( pcsDir / 2 ))
+
+        #cleanup
+        kubectl get pods --no-headers=true | awk '/upf|amf|bsf|pcf|udm|ausf|nrf|nssf|udr|smf/{print $1}'| xargs  kubectl delete pod
+
+        sleep 60
+
+
+        #start-ran
+        bash /opt/scripts/runNodeCmd.sh "nr-gnb -c /opt/UERANSIM/config/open5gs/gnb.yaml > /dev/null 2>&1 &" 11 13
+
+        #bash /opt/scripts/runNodeCmd.sh "/opt/scripts/launchUeSim.py > /dev/null 2>&1 &" 12 14
+
+
+        #start-monitoring
+        #kubectl exec $mongoPod -- bash -c "/scripts/mongoMonitor.py" &
+        mongoPodIp=$(kubectl get pod $mongoPod --template={{.status.podIP}})
+        curl --verbose --request POST --header "Content-Type:application/json" --data '{"expDir":"'$experimentDir'","subExpDir":"'$pcsDir'","runTime":30}' http://$mongoPodIp:15692
+        
+        bash /opt/scripts/startTop.sh $numWorkerNodes $experimentDir $pcsDir
+
+
+        #start-ue
+        for ueNodeIp in "${ueNodes[@]}"
+        do
+            if (( pcsDir > 500 )); then
+                sleep 0.3
+            else
+                sleep 0.2
+            fi
+            echo "UE-SIM IP Address is $ueNodeIp"
+            curl --verbose --request POST --header "Content-Type:application/json" --data '{"numSessions":"'$numSessions'","expDir":"'$experimentDir'","subExpDir":"'$pcsDir'"}'  http://$ueNodeIp:15692
+        done
+
+        sleep 15
+
+
+        #stop-monitoring
+        bash /opt/scripts/stopTop.sh $numWorkerNodes
+
+        bash /opt/scripts/savePodLogs.sh $experimentDir $pcsDir
+
+        sleep 30
+
+        #stop-ran
+        bash /opt/scripts/runNodeCmd.sh "pkill -f nr-ue" 12 14
+
+        sleep 5
+
+        bash /opt/scripts/runNodeCmd.sh "pkill -f nr-gnb" 11 13
+
+        sleep 5
+        
+        #bash /opt/scripts/runNodeCmd.sh "pkill -f launchUeSim.py" 12 14
+
+        #sleep 5
+
+        #kubectl exec -it $mongoPod -- bash -c "pkill -f mongoMonitor.py"
+
+        #sleep 5
+
     done
-
-    sleep 15
-
-
-    #stop-monitoring
-    bash /opt/scripts/stopTop.sh $numWorkerNodes
-
-    bash /opt/scripts/savePodLogs.sh $experimentDir $pcsDir
-
-    sleep 30
-
-    #stop-ran
-    bash /opt/scripts/runNodeCmd.sh "pkill -f nr-ue" 12 14
-
-    sleep 5
-
-    bash /opt/scripts/runNodeCmd.sh "pkill -f nr-gnb" 11 13
-
-    sleep 5
-    
-    #bash /opt/scripts/runNodeCmd.sh "pkill -f launchUeSim.py" 12 14
-
-    #sleep 5
-
-    #kubectl exec -it $mongoPod -- bash -c "pkill -f mongoMonitor.py"
-
-    #sleep 5
-
 done
--- topFile.sh
+++ topFile.sh
@@ -2,8 +2,16 @@
 Hostname=$(hostname --short)
 while true
 do
-        date +"%F--%T.%N" >> top_data_$Hostname.txt
-        COLUMNS=500 top -b -n 1 >> top_data_$Hostname.txt 2>&1 &
         echo " " >> top_data_$Hostname.txt
-        sleep 0.2
+        date +"%F--%T.%N" >> top_data_$Hostname.txt
+        pids=( $(top -b -n 1 | grep "open5gs\|docker\|kube\|calico\|container\|bash\|python" | awk '{print $1}') )
+        numpids=${#pids[@]}
+        topcmd="top -b -n 1"
+        for i in "${pids[@]}"
+        do
+            topcmd="$topcmd -p $i"
+        done
+        COLUMNS=500 $topcmd >> top_data_$Hostname.txt 2>&1 &
+        echo " " >> top_data_$Hostname.txt
+        #sleep 0.1
 done
