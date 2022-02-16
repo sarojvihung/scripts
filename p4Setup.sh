a382f54e03215b7c97c2cc47d5a5eff2b81f518b
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2022-02-16 02:42:52 -0500

    Update p4Setup.sh

--- p4Setup.sh
+++ p4Setup.sh
@@ -1,3 +1,19 @@
+#Auto-method
+sudo apt-get install cmake g++ git automake libtool libgc-dev bison flex \
+libfl-dev libgmp-dev libboost-dev libboost-iostreams-dev \
+libboost-graph-dev llvm pkg-config python3 python3-pip \
+tcpdump
+
+pip3 install ipaddr scapy ply
+
+. /etc/os-release
+echo "deb http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
+curl -L "http://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+sudo apt-get update
+sudo apt install p4lang-p4c
+
+
+#Manual method
 apt-get update && apt-get -y upgrade && apt-get update
 
 sudo apt-get install cmake g++ git automake libtool libgc-dev bison flex \
