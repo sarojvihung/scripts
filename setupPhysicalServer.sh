d6f3c296bf84486b0241155ebbb942c74b8d34bc
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2023-10-15 00:07:29 -0400

    Update setupPhysicalServer.sh

--- setupPhysicalServer.sh
+++ setupPhysicalServer.sh
@@ -19,8 +19,8 @@ pip3 install -U h2
 apt-get -y update
 apt-get install -y ca-certificates
 apt-get install -y apt-transport-https
-curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get -y update
 apt-get install -y kubectl kubelet kubeadm
 
