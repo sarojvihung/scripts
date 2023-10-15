0d9cabe0a791f18395a2207a10332ede63165b7e
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2023-10-15 00:54:34 -0400

    Update configVm.sh

--- configVm.sh
+++ configVm.sh
@@ -24,8 +24,8 @@ rm -f /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 apt-get -y update
 apt-get install -y ca-certificates
 apt-get install -y apt-transport-https
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get -y update
 apt-get install -y kubectl kubelet kubeadm
 
