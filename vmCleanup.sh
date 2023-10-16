d3409bc98ff680deb288b8a827b40825ad7a1c5a
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2023-10-15 23:07:34 -0400

    Update vmCleanup.sh

--- vmCleanup.sh
+++ vmCleanup.sh
@@ -5,6 +5,7 @@ sudo virsh shutdown worker1
 sudo virsh shutdown worker2
 sudo virsh shutdown worker3
 sudo virsh shutdown worker4
+sudo virsh shutdown worker5
 sudo virsh shutdown ran
 
 echo "Waiting for 30 seconds for VMs to shut down..."
@@ -15,6 +16,7 @@ sudo virsh undefine worker1
 sudo virsh undefine worker2
 sudo virsh undefine worker3
 sudo virsh undefine worker4
+sudo virsh undefine worker5
 sudo virsh undefine ran
 
 rm -rf /var/lib/libvirt/images/*
